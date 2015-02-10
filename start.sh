0f48677fed54916b6113a26aeedc9c2ce7ad3aef
phynias <phynias@gmail.com>
2015-02-10 20:30:08 +0000

    fixed sym link

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d "/phpmemcachedadmin/Config" ]; then 
     mv /usr/share/nginx/html/Config /phpmemcachedadmin
-    ln -s /phpmemcachedadmin/html/Config /usr/share/nginx/html/
+    ln -s /phpmemcachedadmin/Config /usr/share/nginx/html/
 else
     rm -fr /usr/share/nginx/html/Config
-    ln -s /phpmemcachedadmin/html/Config /usr/share/nginx/html/
+    ln -s /phpmemcachedadmin/Config /usr/share/nginx/html/
 fi
 
 /usr/sbin/php5-fpm -D && /usr/sbin/nginx
